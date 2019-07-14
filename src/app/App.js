f6fea5a435809a2eaf2cd09c3710f3d015623901
TDSFugal <tim.smith@fugaltech.com>
2019-07-13 23:33:12 -0400

    Restructured directories

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/App.css';
+
+import { Header, Reader } from '../components';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Reader />
+    </div>
+  );
+}
+
+export default App;
