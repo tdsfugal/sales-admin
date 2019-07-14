f10f71b9f88217f8ffdfd5adddd73ff2dde0bf5c
TDSFugal <tim.smith@fugaltech.com>
2019-07-14 02:01:07 -0400

    Reading CSV data into workspace from drop

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -6,44 +6,49 @@ export default class Workspace extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      csvData: 'some data',
+      csvData: null,
     };
-  }
 
-  onDragover(ev) {
-    ev.preventDefault();
-    ev.stopPropagation();
-  }
+    this.onDragEnter = ev => {
+      ev.preventDefault();
+      ev.stopPropagation();
+    };
 
-  onDrop(ev) {
-    ev.preventDefault();
-    ev.stopPropagation();
-    console.log('===== Drop =======');
+    this.onDragOver = ev => {
+      ev.preventDefault();
+      ev.stopPropagation();
+    };
 
-    if (ev.dataTransfer.items) {
-      console.log('with items');
-      // Use DataTransferItemList interface to access the file(s)
-      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-        // If dropped items aren't files, reject them
-        if (ev.dataTransfer.items[i].kind === 'file') {
-          var file = ev.dataTransfer.items[i].getAsFile();
-          console.log('... file[' + i + '].name = ' + file.name);
+    this.onDrop = ev => {
+      ev.preventDefault();
+      ev.stopPropagation();
+      if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+          // If dropped items aren't files, reject them
+          if (ev.dataTransfer.items[i].kind === 'file') {
+            const file = ev.dataTransfer.items[i].getAsFile();
+            this.loadCsvFile(file);
+          }
+        }
+      } else {
+        // Use DataTransfer interface to access the file(s)
+        for (var j = 0; j < ev.dataTransfer.files.length; j++) {
+          const file = ev.dataTransfer.files[j];
+          this.loadCsvFile(file);
         }
       }
-    } else {
-      console.log('with files');
-      // Use DataTransfer interface to access the file(s)
-      for (var j = 0; j < ev.dataTransfer.files.length; j++) {
-        const file = ev.dataTransfer.files[j];
-        console.log('... file[' + j + '].name = ' + file.name);
-      }
-    }
+    };
+
+    this.loadCsvFile = file => {
+      const reader = new FileReader();
+      reader.onload = () => this.setState({ csvData: reader.result });
+      reader.readAsText(file);
+    };
   }
 
   render() {
     const { csvData } = this.state;
-    console.log(csvData);
-
     if (csvData) {
       return (
         <div className="Workspace">
@@ -54,7 +59,8 @@ export default class Workspace extends Component {
       return (
         <div
           className="Workspace"
-          onDragOver={this.onDragover}
+          onDragEnter={this.onDragEnter}
+          onDragOver={this.onDragOver}
           onDrop={this.onDrop}
         >
           <h1>Drop CSV file here</h1>
